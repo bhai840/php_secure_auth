ea0c425faed3404d1e489e32deaa1f2fc8f3c8fa
Jody Bruchon <jody@c02ware.com>
2015-10-30 00:16:42 -0400

    Remove unnecessary header stuff that might cause people trouble

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
 <?php
-// Universal page header code
+// Universal php_sa page header code
 require_once("config.php");
 require_once("db_connect.php");
 require_once("validate_session.php");
-
-print "<!DOCTYPE html>\n";
-print "<html><head>\n";
-print "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
 ?>
