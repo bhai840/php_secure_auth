e528f8c2fe83589287eb528db37c92016d5af94d
Jody Bruchon <jody@jodybruchon.com>
2016-11-23 01:30:52 -0500

    Add "let me in" code to set up a default admin user and password

---
+++ letmein.php
@@ -0,0 +1,5 @@
+<?php
+require_once("lib_auth.php");
+//update_login_entry("admin", "password", "The Super User");
+echo "If you un-commented the command to do so, the admin user's password has been reset.";
+?>
